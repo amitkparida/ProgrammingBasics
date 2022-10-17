0a29f6c1c7e09621ed151a5a9a361db45d25a248
Amit Kumar Parida <aamit.parida@outlook.com>
2022-10-18 02:56:22 +0530

    new methods added

--- DataStructures/Queue.cpp
+++ DataStructures/Queue.cpp
@@ -1,4 +1,4 @@
-//Implementation of Stack using LinkedList
+/* //Implementation of Queue using LinkedList
 
 #include <iostream>
 using namespace std;
@@ -132,11 +132,135 @@ int main() {
 	q.Print();
 
 	return 0;
+} */
+
+
+
+
+
+//Implementation of Queue using Array
+
+#include <iostream>
+using namespace std;
+
+class CQueue {
+private:
+	int* arr;
+	int front;
+	int rear;
+	int capacity;
+
+public:
+	CQueue(int size = 100);
+	~CQueue();
+	void Push(int data);
+	void Pop();
+	int Front();
+	int Back();
+	void Print();
+	int Size();
+	bool IsEmpty();
+};
+
+CQueue::CQueue(int size) {
+	arr = new int[size];
+	capacity = size;
+	front = -1;
+	rear = -1;
+}
+
+CQueue::~CQueue() {
+	delete[] arr;
+}
+
+void CQueue::Push(int data) {
+	if (rear == capacity - 1) {
+		cout << "Queue is full" << endl;
+		return;
+	}
+
+	if (front == -1 && rear == -1)
+	{
+		front = rear = 0;
+	}
+	else
+	{
+		rear++;
+	}
+
+	arr[rear] = data;
+}
+
+void CQueue::Pop() {
+	if (front == -1 || front > rear) {
+		cout << "Queue is empty" << endl;
+		return;
+	}
+
+	if (front == 0 && rear == 0) //In case of single element
+	{
+		front = rear = -1;
+	}
+	else
+	{
+		front++;
+	}
+}
+
+int CQueue::Front() {
+	if (front == -1 || front > rear) {
+		cout << "Queue is empty" << endl;
+		return -1;
+	}
+
+	return arr[front];
+}
+
+int CQueue::Back() {
+	if (front == -1 || front > rear) {
+		cout << "Queue is empty" << endl;
+		return -1;
+	}
+
+	return arr[rear];
 }
 
 
+void CQueue::Print() {
+	if (front == -1 || front > rear) {
+		cout << "Queue is empty" << endl;
+		return;
+	}
 
+	for (int i = front; i <= rear; i++)
+	{
+		cout << arr[i] << " ";
+	}
 
+	cout << endl;
+}
+
+int CQueue::Size() {
+	return rear + 1;
+}
+
+bool CQueue::IsEmpty() {
+	return (rear == -1);
+}
+
+int main() {
+	CQueue q;
+	q.Push(1);
+	q.Push(2);
+	q.Push(3);
+	q.Print();
+	cout << q.Front() << endl;
+	cout << q.Back() << endl;
+	q.Pop();
+	cout << q.Front() << endl;
+	cout << q.Back() << endl;
+	return 0;
+}
 
 
 
--- DataStructures/Stack.cpp
+++ DataStructures/Stack.cpp
@@ -1,4 +1,5 @@
-//Implementation of Stack using LinkedList
+
+/* //Implementation of Stack using LinkedList
 
 #include <iostream>
 using namespace std;
@@ -141,12 +142,100 @@ int main() {
 	st.Print();
 
 	return 0;
+}*/
+
+
+
+
+
+//Implement Using dynamic Array
+#include <iostream>
+using namespace std;
+
+class CStack {
+private:
+	int* arr;
+	int top;
+	int capacity;
+
+public:
+	CStack(int size = 100);
+	~CStack();
+	void Push(int data);
+	void Pop();
+	int Top();
+	void Print();
+	int Size();
+	bool IsEmpty();
+};
+
+CStack::CStack(int size) {
+	arr = new int[size];
+	capacity = size;
+	top = -1;
 }
 
+CStack::~CStack() {
+	delete[] arr;
+}
 
+void CStack::Push(int data) {
+	if (top == capacity - 1) {
+		cout << "Stack OverFlow" << endl;
+		return;
+	}
+	top++;
+	arr[top] = data;
+}
 
+void CStack::Pop() {
+	if (top == -1) {
+		cout << "Stack Underflow" << endl;
+		return;
+	}
 
+	top--;
+}
 
+int CStack::Top() {
+	if (top == -1) {
+		cout << "Stack Underflow" << endl;
+		return -1;
+	}
+	return arr[top];
+}
 
+void CStack::Print() {
 
+	if (top == -1) {
+		cout << "Stack Underflow" << endl;
+		return;
+	}
 
+	for (int i = top; i >= 0; i--)
+	{
+		cout << arr[i] << " ";
+	}
+	
+	cout << endl;
+}
+
+int CStack::Size() {
+	return top + 1;
+}
+
+bool CStack::IsEmpty() {
+	return (top == -1);
+}
+
+int main() {
+	CStack s;
+	s.Push(1);
+	s.Push(2);
+	s.Push(3);
+	s.Print();
+	cout << s.Top() << endl;
+	s.Pop();
+	cout << s.Top() << endl;
+	return 0;
+}
