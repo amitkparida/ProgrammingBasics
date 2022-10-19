a51490d399387a51c388987d083390584cf7b37e
Amit Kumar Parida <aamit.parida@outlook.com>
2022-10-20 02:01:27 +0530

    .

--- DataStructures/DFS.cpp
+++ DataStructures/DFS.cpp
@@ -114,8 +114,6 @@ STEPS:
 4. Go to Step 2
 */
 
-
-
 #include <iostream>
 #include <stack>
 #include <vector>
@@ -124,6 +122,15 @@ using namespace std;
 vector<vector<int>> Vec;
 vector<bool> visited;
 
+void dfs_r(int s) {
+    visited[s] = true;
+    cout << s << " ";
+    for (int i = 0; i < Vec[s].size(); ++i) {
+        if (visited[Vec[s][i]] == false)
+            dfs_r(Vec[s][i]);
+    }
+}
+
 void dfs(const vector<vector<int>>& Vec, int s) {
     stack<int> Stk;
     Stk.push(s);
@@ -131,7 +138,7 @@ void dfs(const vector<vector<int>>& Vec, int s) {
     while (!Stk.empty()) {
         int node = Stk.top();
         Stk.pop();
-        cout << "Visited: " << node << " ";
+        cout << node << " ";
         for (int i = 0; i < Vec[node].size(); ++i) {
             if (!visited[Vec[node][i]]) {
                 Stk.push(Vec[node][i]);
@@ -141,7 +148,41 @@ void dfs(const vector<vector<int>>& Vec, int s) {
     }
 }
 
-int main()
-{
+
+void initialize() {
+    for (int i = 0; i < visited.size(); ++i)
+        visited[i] = false;
+}
+
+int main() {
+    int nodes, edges, x, y;
+    cin >> nodes;
+    cin >> edges;
+    Vec.resize(nodes + 1);
+    visited.resize(nodes + 1);
+    for (int i = 0; i < edges; ++i)
+    {
+        cin >> x >> y;
+        Vec[x].push_back(y);
+        Vec[y].push_back(x);
+    }
+
+    initialize();
+    dfs_r(1); 
+    cout << endl;
+
+    initialize();
+    dfs(Vec, 1);
     return 0;
 }
+
+//Input format
+//6 7
+//1 2
+//1 4
+//4 5
+//2 4
+//2 3
+//3 6
+//4 6
+
