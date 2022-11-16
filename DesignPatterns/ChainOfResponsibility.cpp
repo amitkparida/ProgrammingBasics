f88b5f87561aca0920bca82dd34aa7a546635719
Amit Kumar Parida <aamit.parida@outlook.com>
2022-11-16 13:52:11 +0530

    .

--- DesignPatterns/ChainOfResponsibility.cpp
+++ DesignPatterns/ChainOfResponsibility.cpp
@@ -1,7 +1,141 @@
 
-// https://www.youtube.com/watch?v=2Vh34iOzrt4&list=PLJhxHp6j1l-8_kjEL2hVJDs0_4eJbHtWH&index=8
+// Chain of Responsibility:
+// 1. Used when there are interdependent modules and each of them performs a specific task or part of some task.
+// 2. For such cases, a chain of modules is createdand each module knows the next module in chain.
+// 3. If the request cannot be performed by one module then it passes the control to next module.
+// 4. This keeps going on till a correct handler is found to handle the request.
 
+
+// EXAMPLE: (ATM)
 // https://www.youtube.com/watch?v=uIDfCzAAU6c
+// In an ATM, we have 500 Rs bills, 100 Rs bills, 2000 Rs currency bill, they all are placed in different slots.
+// 
+// If someone puts a request of 500 Rs bill, then ATM checks the slots one by oneand if one module is not to dispense for 500, 
+// then it passes to next module to let that module check if he can fulfill the requestand so onand ultimately at 500 slot, 
+// it stopsand that module performs the real processing(i.e.to dispense the cash).
+
+#include<iostream>
+using namespace std;
+
+#define CASH_DISPENSE	1
+#define INVALID_REQUEST	0
+
+#define BILL_500		500
+#define BILL_1000		1000
+#define BILL_2000		2000
+
+class ATMModule {
+protected:
+	int m_nReqType;
+	ATMModule* pNext;
+
+public:
+	ATMModule(int nReqType) {
+		m_nReqType = nReqType;
+	}
+
+	void SetNextSlot(ATMModule* pNext)
+	{
+		this->pNext = pNext;
+	}
+
+	virtual int Run(int nCurrency) = 0;
+};
+
+class Slot_500 :public ATMModule {
+public:
+	Slot_500() : ATMModule(BILL_500) {
+		pNext = NULL;
+	}
+
+	int Run(int nCurrency) override {
+		if (m_nReqType == nCurrency) {
+			cout << "Slot_500 is the right slot" << endl;
+			return CASH_DISPENSE;
+		}
+		else {
+			cout << "Slot_500 is not the right slot. Moving to next Slot_1000." << endl;
+			if (pNext) {
+				return pNext->Run(nCurrency);
+			}
+			else {
+				cout << "Invalid Request." << endl;
+				return INVALID_REQUEST;
+			}
+		}
+	}
+};
+
+class Slot_1000 :public ATMModule {
+public:
+	Slot_1000() : ATMModule(BILL_1000) {
+		pNext = NULL;
+	}
+
+	int Run(int nCurrency) override {
+		if (m_nReqType == nCurrency) {
+			cout << "Slot_1000 is the right slot." << endl;
+			return CASH_DISPENSE;
+		}
+		else {
+			cout << "Slot_1000 is not the right slot. Moving to next Slot_2000." << endl;
+			if (pNext) {
+				return pNext->Run(nCurrency);
+			}
+			else {
+				cout << "Invalid Request." << endl;
+				return INVALID_REQUEST;
+			}
+		}
+	}
+};
+
+class Slot_2000 :public ATMModule {
+public:
+	Slot_2000() : ATMModule(BILL_2000) {
+		pNext = NULL;
+	}
+
+	int Run(int nCurrency) override {
+		if (m_nReqType == nCurrency) {
+			cout << "Slot_2000 is the right slot." << endl;
+			return CASH_DISPENSE;
+		}
+		else {
+			cout << "Slot_2000 is not the right slot." << endl;
+			if (pNext) {
+				return pNext->Run(nCurrency);
+			}
+			else {
+				cout << "Invalid Request." << endl;
+				return INVALID_REQUEST;
+			}
+		}
+	}
+};
+
+int main() {
+	ATMModule* p1 = new Slot_500;
+	ATMModule* p2 = new Slot_1000;
+	ATMModule* p3 = new Slot_2000;
+
+	p1->SetNextSlot(p2);
+	p2->SetNextSlot(p3);
+
+	p1->Run(3000);
+
+	return 0;
+}
+
+
+
+
+// ==================================================================================
+
+
+/*
+// Another EXAMPLE: (Interview process)
+// https://www.youtube.com/watch?v=2Vh34iOzrt4&list=PLJhxHp6j1l-8_kjEL2hVJDs0_4eJbHtWH&index=8
 
 #include<iostream>
 using namespace std;
@@ -168,3 +302,4 @@ int main() {
 
 	hc->ProceedWithInterview();
 }
+*/
