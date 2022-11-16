6b5a31f1fc234769d731d5eb80ff4f930c7e8ed4
Amit Kumar Parida <aamit.parida@outlook.com>
2022-11-17 00:23:54 +0530

    .

--- STL/AssociativeContainers/NOTES.cpp
+++ STL/AssociativeContainers/NOTES.cpp
@@ -5,19 +5,14 @@ set and map are implemented using Red-Black tree. Operations are O(logn). Elemen
 
 unordered_set and unordered_map are implemented using Hash Tables. Operations are O(1). Elements are not sorted. 
 
-
 -------------------
 Why Red-Black tree is used in set and map, not any other tree?
 
 Binary search tree operations are also O(logn) in average case, but in worst case if the BST is skewed, the operation will be O(n).
-
-AVL tree is self balancing binary tree and it's operations are O(logn). But the balancing is done more frequently in AVL tree which takes time.
-
-Red-Black tree is like AVL tree, but it does balancing less frequently.
-
-
-The AVL trees are more balanced compared to Red-Black Trees, but they may cause more rotations during insertion and deletion. So if your application involves frequent insertions and deletions, then Red-Black trees should be preferred. And if the insertions and deletions are less frequent and search is a more frequent operation, then AVL tree should be preferred over the Red-Black Tree.
-
+AVL tree is self balancing binary tree and it's operations are O(logn). Red-Black tree's operations are also O(logn). 
+The AVL trees are more balanced compared to Red-Black Trees, but they may cause more rotations during insertion and deletion. 
+So if your application involves frequent insertions and deletions, then Red-Black trees should be preferred. 
+And if the insertions and deletions are less frequent and search is a more frequent operation, then AVL tree should be preferred over the Red-Black Tree.
 
 ------------
 
