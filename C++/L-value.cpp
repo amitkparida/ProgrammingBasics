c7006c0cb012c0cd9cae2a79f62e90a944e57cf6
Amit Kumar Parida <aamit.parida@outlook.com>
2022-11-22 12:28:36 +0530

    .

--- C++/L-value.cpp
+++ C++/L-value.cpp
@@ -1,9 +1,15 @@
+
+//Every expression in C++ is either Lvalue or Rvalue expression.
+//Lvalue: If you can take address of the expression then it is Lvalue, and they last extended period of time.
+//Rvalue: Rvalues are such expression which you cannot take address of. They are temporary and don't exist after one line.
+
 #include <iostream>
 
 //Returns r-value
 int Add(int x, int y) {
 	return x + y;
 }
+
 //Return l-value
 int& Transform(int& x) {
 	x *= x;
@@ -13,31 +19,37 @@ int& Transform(int& x) {
 void Print(int& x) {
 	std::cout << "Print(int&)" << std::endl;
 }
+
 void Print(const int& x) {
 	std::cout << "Print(const int&)" << std::endl;
-
 }
+
 void Print(int&& x) {
 	std::cout << "Print(int &&)" << std::endl;
 }
+
 int main() {
 	//x is lvalue
 	int x = 10;
 
 	//ref is l-value reference
 	int& ref = x;
+
 	//Transform returns an l-value
 	int& ref2 = Transform(x);
+
 	//Binds to function that accepts l-value reference
-	Print(x);
+	Print(x); //Print(const int&)
 
 	//rv is r-value reference
 	int&& rv = 8;
 
 	//Add returns a temporary (r-value)
 	int&& rv2 = Add(3, 5);
+
 	//Binds to function that accepts a temporary, i.e. r-value reference
-	Print(3);
+	Print(3); //Print(int &&)
+
 	return 0;
 }
 
