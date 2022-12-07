ad7adeade0b075b55898654b2f05002b624c5f16
Amit Kumar Parida <aamit.parida@outlook.com>
2022-12-07 15:37:44 +0530

    .

--- STL/ContainerAdaptors/1 stack.cpp
+++ STL/ContainerAdaptors/1 stack.cpp
@@ -18,21 +18,78 @@
 #include <stack>
 using namespace std;
 
-void print(stack<int> stk) {
-    while (!stk.empty()) {
-        cout << stk.top() << endl;
-        stk.pop(); // Remove from the back
-    }
+//void print(stack<int> stk) {
+//    while (!stk.empty()) {
+//        cout << stk.top() << endl;
+//        stk.pop(); // Remove from the back
+//    }
+//}
+//
+//int main() {
+//    stack<int> stk;
+//
+//    stk.push(2);
+//    stk.push(3);
+//    stk.push(4);
+//
+//    print(stk);
+//
+//    return 0;
+//}
+
+
+//Reverse string using std::stack
+void reverse(char* str)
+{
+	stack<char> s;
+	int len = strlen(str);
+	for (int i = 0; i < len; i++) {
+		s.push(str[i]);
+	}
+
+	for (int i = 0; i < len; i++) {  //Note that here i < s.size() will not work in the condition as size changes after each Pop().
+		str[i] = s.top();
+		s.pop();
+	}
+	//str[len] = '\0'; //not needed as str already has \0 at the end
+
+	//int i = 0;
+	//while (!s.empty()) {
+	//	str[i++] = s.top();
+	//	s.pop();
+	//}
+	////str[i] = '\0'; //not needed as str already has '\0' at the end
 }
 
-int main() {
-    stack<int> stk;
+//Reverse print a string using std::stack
+void reversePrint(char* str)
+{
+	stack<char> s;
+	int len = strlen(str);
+	for (int i = 0; i < len; i++) {
+		s.push(str[i]);
+	}
 
-    stk.push(2);
-    stk.push(3);
-    stk.push(4);
+	while (!s.empty()) {
+		cout << s.top();
+		s.pop();
+	}
 
-    print(stk);
-
-    return 0;
+	//for (int i = 0; i < len; i++) {  //Note that here s.size() will not work in the condition as size changes after each Pop().
+	//	cout << s.top();
+	//	s.pop();
+	//}
 }
+
+int main()
+{
+	char str[]{"Amit Kumar Parida"};
+	printf("\nBefore reversing:	%s\n", str);
+	reverse(str);
+	printf("After reversing:	%s\n", str);
+
+	reversePrint(str);
+}
+
+
+
