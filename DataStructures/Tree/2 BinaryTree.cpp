8d5a6acbcb8655733030e3065e785154ad19db1d
Amit Kumar Parida <aamit.parida@outlook.com>
2022-12-17 01:22:11 +0530

    .

--- DataStructures/Tree/2 BinaryTree.cpp
+++ DataStructures/Tree/2 BinaryTree.cpp
@@ -21,6 +21,7 @@
 // Watch this to understand the code: https://www.youtube.com/watch?v=5NiXlPrLslg&list=PLDzeHZWIZsTo87y1ytEAqp7wYlEP3nner
 #include<iostream>
 #include<queue>
+#include<stack>
 using namespace std;
 
 struct Node {
@@ -199,6 +200,172 @@ int countLeafNodes1(Node* root) {
     return countLeafNodes1(root->left) + countLeafNodes1(root->right);
 }
 
+// https://www.youtube.com/watch?v=xtuhK-a18is
+void zigZagTraversal(Node* root) {
+    if (root == NULL) 
+        return;
+
+    stack<Node*> currLevel;
+    stack<Node*> nextLevel;
+
+    bool leftToRight = true;
+
+    currLevel.push(root);
+
+    while (!currLevel.empty()) {
+        Node* temp = currLevel.top();
+        currLevel.pop();
+
+        if (temp) {
+            cout << temp->data << " ";
+
+            if (leftToRight) {
+                if (temp->left) {
+                    nextLevel.push(temp->left);
+                }
+                if (temp->right) {
+                    nextLevel.push(temp->right);
+                }
+            }
+            else { //RightToLeft
+                if (temp->right) {
+                    nextLevel.push(temp->right);
+                }
+                if (temp->left) {
+                    nextLevel.push(temp->left);
+                }
+            }
+        }
+
+        if (currLevel.empty()) {
+            leftToRight = !leftToRight;
+            swap(currLevel, nextLevel);
+        }
+    }
+}
+
+// We can use deque also instead of using two stacks
+void zigZagTraversal1(Node* root) {
+    deque<Node*> Q;
+    Q.push_back(root);
+    Q.push_back(NULL);
+    bool ltr = true;
+    while (Q.size() > 1) {
+        if (ltr) {
+            Node* n = Q.front();
+            if (n) {
+                std::cout << n->data << " ";
+                if (n->left)Q.push_back(n->left);
+                if (n->right)Q.push_back(n->right);
+                Q.pop_front();
+            }
+            else { ltr = !ltr; }
+        }
+        else {
+            Node* n = Q.back();
+            if (n) {
+                std::cout << n->data << " ";
+                if (n->right)Q.push_front(n->right);
+                if (n->left)Q.push_front(n->left);
+                Q.pop_back();
+            }
+            else { ltr = !ltr; }
+        }
+    }
+}
+
+
+// For the above solution, you can just perform the simple level order traversal.Considering 1 based indexing
+// If level is odd then then just print the elements.
+//If level is even just use a stack and push the elements into it and once level is traversed start popping and print the values. You will get values in reverse order because of LIFO nature of Stack.
+//And finally check at the end if stack still contains elements, empty the stack and print the element.
+vector<int> zigZagTraversal2(Node* root)
+{
+    queue<Node*> q;
+    stack<int> st;
+    vector<int> ans;
+    q.push(root);
+    q.push(NULL);
+    int level = 1;
+    while (!q.empty())
+    {
+        Node* temp = q.front();
+        q.pop();
+        if (temp != NULL)
+        {
+            if (level % 2 == 1)
+            {
+                ans.push_back(temp->data);
+            }
+            else
+            {
+                st.push(temp->data);
+            }
+
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+        else if (!q.empty())
+        {
+            q.push(NULL);
+            while (!st.empty())
+            {
+                ans.push_back(st.top());
+                st.pop();
+            }
+            level++;
+        }
+    }
+    while (!st.empty())
+    {
+        ans.push_back(st.top());
+        st.pop();
+    }
+    return ans;
+}
+
+
+//Striver: https://takeuforward.org/data-structure/zig-zag-traversal-of-binary-tree/
+vector <vector<int>> zigZagTraversal3(Node* root) {
+    vector<vector<int>> result;
+    if (root == NULL) {
+        return result;
+    }
+
+    queue < Node* > q;
+    q.push(root);
+    bool leftToRight = true;
+
+    while (!q.empty()) {
+        int size = q.size();
+        vector < int > row(size);
+        for (int i = 0; i < size; i++) {
+            Node* node = q.front();
+            q.pop();
+
+            // find position to fill node's value
+            int index = (leftToRight) ? i : (size - 1 - i);
+
+            row[index] = node->data;
+            if (node->left) {
+                q.push(node->left);
+            }
+            if (node->right) {
+                q.push(node->right);
+            }
+        }
+        // after this level
+        leftToRight = !leftToRight;
+        result.push_back(row);
+    }
+    return result;
+}
 
 int main() {
 
@@ -209,10 +376,18 @@ int main() {
 //     7   11   17
 
 
-    Node* root = NULL;
-
     //creating a Tree
-    root = buildTree(root); // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    
+    //Node* root = NULL;
+    //root = buildTree(root); // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+
+    Node* root = new Node(1);
+    root->left = new Node(3);
+    root->right = new Node(5);
+    root->left->left = new Node(7);
+    root->left->right = new Node(11);
+    root->right->right = new Node(17);
+
 
     cout << "\nLevel order traversal is: ";
     levelOrderTraversal(root); // 1 3 5 7 11 17
@@ -229,6 +404,24 @@ int main() {
     cout << "Post order traversal is: ";
     postorder(root); // 7 11 3 17 5 1
     cout << endl;
+
+    cout << "Zig Zag traversal is: ";
+    zigZagTraversal1(root); // 1 5 3 7 11 17
+    cout << endl;
+
+    cout << "Zig Zag traversal is: ";
+    for (auto e : zigZagTraversal2(root)) {
+            cout << e << " ";
+    }
+    cout << endl;
+
+    cout << "Zig Zag traversal is: " << endl;
+    for (auto vec: zigZagTraversal3(root)) {
+        for (auto e: vec) {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
 
     cout << "Leaf nodes are: ";
     int count = countLeafNodes(root); // 7 11 17
