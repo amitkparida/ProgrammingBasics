5e001ce5e61678988d62cdb8b9371bb23d42ab44
Amit Kumar Parida <aamit.parida@outlook.com>
2022-12-24 23:01:31 +0530

    .

---
+++ DataStructures/3 Stack_Queue.cpp
@@ -0,0 +1,63 @@
+//Implement Stack using Queue
+
+#include <string>
+#include <queue>
+#include <iostream>
+
+class Stack {
+private:
+    std::queue<int>* que1 = new std::queue<int>();
+    std::queue<int>* que2 = new std::queue<int>();
+    int size = 0;
+
+public:
+    virtual void push(int value);
+    virtual int pop();
+    virtual int pop2();
+};
+
+
+void Stack::push(int value) {
+    que1->push(value);
+    size += 1;
+}
+
+int Stack::pop() {
+    int value = 0, s = size;
+    while (s > 0) {
+        value = que1->front();
+        que1->pop();
+        if (s > 1)
+            que2->push(value);
+        s--;
+    }
+    std::queue<int>* temp = que1;
+    que1 = que2;
+    que2 = temp;
+    size -= 1;
+    return value;
+}
+
+int Stack::pop2() {
+    int value = 0, s = size;
+    while (s > 0) {
+        value = que1->front();
+        que1->pop();
+        if (s > 1)
+            que1->push(value);
+        s--;
+    }
+    size -= 1;
+    return value;
+}
+
+// Testing code.
+int main() {
+    Stack stk;
+    stk.push(1);
+    stk.push(2);
+    stk.push(3);
+    std::cout << "Stack pop : " << stk.pop2() << std::endl;
+    std::cout << "Stack pop : " << stk.pop2() << std::endl;
+    return 0;
+}
---
+++ DataStructures/5 Queue_Stack.cpp
@@ -0,0 +1,55 @@
+//Implementation of Queue using Stack
+
+#include <string>
+#include <stack>
+#include <iostream>
+using namespace std;
+
+class QueueUsingStack {
+private:
+	stack<int> stk1;
+	stack<int> stk2;
+
+public:
+	void add(int value);
+	int remove();
+};
+
+void QueueUsingStack::add(int value) {
+	stk1.push(value);
+}
+
+int QueueUsingStack::remove() {
+	int value;
+	if (stk2.empty() == false) {
+		value = stk2.top();
+		stk2.pop();
+		return value;
+	}
+
+	while (stk1.empty() == false) {
+		value = stk1.top();
+		stk1.pop();
+		stk2.push(value);
+	}
+
+	value = stk2.top();
+	stk2.pop();
+	return value;
+}
+
+// Testing code.
+int main() {
+	QueueUsingStack que;
+	que.add(1);
+	que.add(2);
+	que.add(3);
+	cout << "Queue remove : " << que.remove() << endl;
+	cout << "Queue remove : " << que.remove() << endl;
+	que.add(4);
+	que.add(5);
+	cout << "Queue remove : " << que.remove() << endl;
+
+	return 0;
+}
+
