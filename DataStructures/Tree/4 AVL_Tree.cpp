7ff60c8566df61e1ee53bece438fc4b8651687b0
Amit Kumar Parida <aamit.parida@outlook.com>
2022-12-25 00:21:48 +0530

    .

--- DataStructures/Tree/4 AVL_Tree.cpp
+++ DataStructures/Tree/4 AVL_Tree.cpp
@@ -16,6 +16,7 @@
 //    D. right right   ==>> Left 
 
 
+/*
 // PROGRAM from CppNut
 #include <iostream> 
 using namespace std;
@@ -96,7 +97,7 @@ int getBalance(Node* N)
 
 Node* insert(Node* node, int key)
 {
-    /* 1. Perform the normal BST insertion */
+    // 1. Perform the normal BST insertion 
     if (node == NULL)
         return(newNode(key));
 
@@ -107,11 +108,11 @@ Node* insert(Node* node, int key)
     else // Equal keys are not allowed in BST  
         return node;
 
-    /* 2. Update height of this ancestor node */
+    // 2. Update height of this ancestor node 
     node->height = 1 + max(height(node->left),
         height(node->right));
 
-    /* 3. Get the balance factor of this ancestor node to check whether this node became unbalanced */
+    // 3. Get the balance factor of this ancestor node to check whether this node became unbalanced 
     int balance = getBalance(node);
 
     // Left Left Case  
@@ -133,7 +134,7 @@ Node* insert(Node* node, int key)
         node->right = rightRotate(node->right);
         return leftRotate(node);
     }
-    return node;  /* return the (unchanged) node pointer */
+    return node;  // return the (unchanged) node pointer 
 }
 void preOrder(Node* root) {
     if (root != NULL)
@@ -157,3 +158,321 @@ int main() {
 
     return 0;
 }
+*/
+
+
+
+//From Hemant Jain book
+
+#include <vector>
+#include <iostream>
+
+class AVLTree {
+private:
+    struct Node {
+        int data = 0;
+        Node* left;
+        Node* right;
+        int height = 0;
+
+        ~Node();
+        Node(int d, Node* l, Node* r);
+    };
+
+    Node* root;
+
+public:
+    ~AVLTree();
+    AVLTree();
+    void insert(int data);
+    void deleteNode(int data);
+    void printTree();
+    int max(int a, int b);
+
+private:
+    Node* insert(Node* node, int data);
+    Node* rightRotate(Node* x); // Function to right rotate subtree rooted with x
+    Node* leftRotate(Node* x); // Function to left rotate subtree rooted with x
+    Node* rightLeftRotate(Node* x); // Function to right then left rotate subtree rooted with x
+    Node* leftRightRotate(Node* x); // Function to left then right rotate subtree rooted with x
+    Node* deleteNode(Node* node, int data);
+    Node* findMin(Node* curr);
+    int height(Node* n);
+    int getBalance(Node* node);
+    void printTree(Node* node, std::string indent, bool isLeft);
+};
+
+
+AVLTree::Node::Node(int d, Node* l, Node* r) {
+	data = d;
+	left = l;
+	right = r;
+	height = 0;
+}
+
+AVLTree::Node::~Node() {
+	delete left;
+	delete right;
+}
+
+AVLTree::AVLTree() {
+	root = nullptr;
+}
+
+AVLTree::~AVLTree() {
+	delete root;
+}
+
+int AVLTree::height(Node* n) {
+	if (n == nullptr) {
+		return -1;
+	}
+	return n->height;
+}
+
+int AVLTree::getBalance(Node* node) {
+	return (node == nullptr) ? 0 : height(node->left) - height(node->right);
+}
+
+void AVLTree::insert(int data) {
+	root = insert(root, data);
+}
+
+AVLTree::Node* AVLTree::insert(Node* node, int data) {
+	if (node == nullptr) {
+		return new Node(data, nullptr, nullptr);
+	}
+
+	if (node->data > data) {
+		node->left = insert(node->left, data);
+	}
+	else if (node->data < data) {
+		node->right = insert(node->right, data);
+	}
+	else { // Duplicate data not allowed
+		return node;
+	}
+
+	node->height = max(height(node->left), height(node->right)) + 1;
+	int balance = getBalance(node);
+
+	if (balance > 1) {
+		if (data < node->left->data) // Left Left Case
+		{
+			return rightRotate(node);
+		}
+		if (data > node->left->data) // Left Right Case
+		{
+			return leftRightRotate(node);
+		}
+	}
+
+	if (balance < -1) {
+		if (data > node->right->data) // Right Right Case
+		{
+			return leftRotate(node);
+		}
+		if (data < node->right->data) // Right Left Case
+		{
+			return rightLeftRotate(node);
+		}
+	}
+	return node;
+}
+
+AVLTree::Node* AVLTree::rightRotate(Node* x) {
+	Node* y = x->left;
+	Node* T = y->right;
+
+	// Rotation
+	y->right = x;
+	x->left = T;
+
+	// Update heights
+	x->height = max(height(x->left), height(x->right)) + 1;
+	y->height = max(height(y->left), height(y->right)) + 1;
+
+	// Return new root
+	return y;
+}
+
+AVLTree::Node* AVLTree::leftRotate(Node* x) {
+	Node* y = x->right;
+	Node* T = y->left;
+
+	// Rotation
+	y->left = x;
+	x->right = T;
+
+	// Update heights
+	x->height = max(height(x->left), height(x->right)) + 1;
+	y->height = max(height(y->left), height(y->right)) + 1;
+
+	// Return new root
+	return y;
+}
+
+AVLTree::Node* AVLTree::rightLeftRotate(Node* x) {
+	x->right = rightRotate(x->right);
+	return leftRotate(x);
+}
+
+AVLTree::Node* AVLTree::leftRightRotate(Node* x) {
+	x->left = leftRotate(x->left);
+	return rightRotate(x);
+}
+
+void AVLTree::deleteNode(int data) {
+	root = deleteNode(root, data);
+}
+
+AVLTree::Node* AVLTree::deleteNode(Node* node, int data) {
+	if (node == nullptr) {
+		return nullptr;
+	}
+
+	if (node->data == data) {
+		if (node->left == nullptr && node->right == nullptr) {
+			return nullptr;
+		}
+		else if (node->left == nullptr) {
+			return node->right; // no need to modify height
+		}
+		else if (node->right == nullptr) {
+			return node->left; // no need to modify height
+		}
+		else {
+			Node* minNode = findMin(node->right);
+			node->data = minNode->data;
+			node->right = deleteNode(node->right, minNode->data);
+		}
+	}
+	else {
+		if (node->data > data) {
+			node->left = deleteNode(node->left, data);
+		}
+		else {
+			node->right = deleteNode(node->right, data);
+		}
+	}
+
+	node->height = max(height(node->left), height(node->right)) + 1;
+	int balance = getBalance(node);
+
+	if (balance > 1) {
+		if (data >= node->left->data) // Left Left Case
+		{
+			return rightRotate(node);
+		}
+		if (data < node->left->data) // Left Right Case
+		{
+			return leftRightRotate(node);
+		}
+	}
+
+	if (balance < -1) {
+		if (data <= node->right->data) // Right Right Case
+		{
+			return leftRotate(node);
+		}
+		if (data > node->right->data) // Right Left Case
+		{
+			return rightLeftRotate(node);
+		}
+	}
+	return node;
+}
+
+AVLTree::Node* AVLTree::findMin(Node* curr) {
+	Node* node = curr;
+	if (node == nullptr) {
+		return nullptr;
+	}
+
+	while (node->left != nullptr) {
+		node = node->left;
+	}
+	return node;
+}
+
+void AVLTree::printTree() {
+	std::string str = "";
+	printTree(root, str, false);
+	std::cout << std::endl;
+}
+
+void AVLTree::printTree(Node* node, std::string indent, bool isLeft) {
+	if (node == nullptr) {
+		return;
+	}
+	if (isLeft) {
+		std::cout << indent << "L:";
+		indent += "|  ";
+	}
+	else {
+		std::cout << indent << "R:";
+		indent += "   ";
+	}
+
+	std::cout << node->data << "(" << node->height << ")" << std::endl;
+	printTree(node->left, indent, true);
+	printTree(node->right, indent, false);
+}
+
+int AVLTree::max(int a, int b) {
+	return (a > b) ? a : b;
+}
+
+// Testing code.
+int main() {
+	AVLTree* t = new AVLTree();
+	t->insert(1);
+	t->insert(2);
+	t->insert(3);
+	t->insert(4);
+	t->insert(5);
+	t->insert(6);
+	t->insert(7);
+	t->insert(8);
+	t->printTree();
+
+	t->deleteNode(5);
+	t->printTree();
+
+	t->deleteNode(1);
+	t->printTree();
+
+	t->deleteNode(2);
+	t->printTree();
+
+	delete t;
+}
+
+/*
+ R:4(3)
+   L:2(1)
+   |  L:1(0)
+   |  R:3(0)
+   R:6(2)
+	  L:5(0)
+	  R:7(1)
+		 R:8(0)
+R:4(2)
+   L:2(1)
+   |  L:1(0)
+   |  R:3(0)
+   R:7(1)
+	  L:6(0)
+	  R:8(0)
+R:4(2)
+   L:2(1)
+   |  R:3(0)
+   R:7(1)
+	  L:6(0)
+	  R:8(0)
+R:4(2)
+   L:3(0)
+   R:7(1)
+	  L:6(0)
+	  R:8(0)
+ */
--- DataStructures/Tree/6 RedBlackTree.cpp
+++ DataStructures/Tree/6 RedBlackTree.cpp
@@ -22,3 +22,479 @@
 //	2) If Parent is Red
 //		a) If Black or absent sibling then rotate then re - color and done.
 //		b) If Red sibling then re - color and check again.
+
+
+
+#include <iostream>
+#include <vector>
+
+class RBTree {
+private:
+	struct Node {
+		Node* left, * right, * parent, * nullNode;
+		int data = 0;
+		bool colour = false; // true for red colour, false for black colour
+		~Node();
+		Node(int data, Node* nullNode);
+	};
+
+	Node* root;
+	Node* nullNode;
+
+public:
+	~RBTree();
+	RBTree();
+	void printTree();
+	void insert(int data);
+	void remove(int data);
+
+private:
+	bool isRed(Node* node); // To check whether node is of colour red or not.
+	Node* getUncle(Node* node);
+	Node* rightRotate(Node* x); // Function to right rotate subtree rooted with x
+	Node* leftRotate(Node* x); // Function to left rotate subtree rooted with x
+	Node* rightLeftRotate(Node* node);
+	Node* leftRightRotate(Node* node);
+	Node* find(int data);
+	void printTree(Node* node, std::string indent, bool isLeft);
+	Node* insert(Node* node, int data);
+	void fixRedRed(Node* x);
+	void remove(Node* node, int key);
+	void fixDoubleBlack(Node* x);
+	Node* getSibling(Node* node);
+	void joinParentChild(Node* u, Node* v);
+	Node* minimum(Node* node);
+};
+
+
+RBTree::Node::Node(int data, Node* nullNode) {
+	this->data = data;
+	this->left = nullNode;
+	this->right = nullNode;
+	this->colour = true; // New node are red in colour.
+	this->parent = nullNode;
+	this->nullNode = nullNode; // used only in delete to prevent double delete.
+}
+
+RBTree::Node::~Node() {
+	if (left != nullNode)
+		delete left;
+	if (right != nullNode)
+		delete right;
+}
+
+RBTree::RBTree() {
+	nullNode = new Node(0, nullptr);
+	nullNode->colour = false;
+	root = nullNode;
+}
+
+RBTree::~RBTree() {
+	delete root;
+	delete nullNode;
+}
+
+bool RBTree::isRed(Node* node) {
+	return (node == nullptr) ? false : (node->colour == true);
+}
+
+void RBTree::printTree() {
+	printTree(root, "", false);
+	std::cout << std::endl;
+}
+
+void RBTree::printTree(Node* node, std::string indent, bool isLeft) {
+	if (node == nullNode) {
+		return;
+	}
+	if (isLeft) {
+		std::cout << indent << "L:";
+		indent += "|  ";
+	}
+	else {
+		std::cout << indent << "R:";
+		indent += "   ";
+	}
+
+	std::cout << node->data << "(" << node->colour << ")" << std::endl;
+	printTree(node->left, indent, true);
+	printTree(node->right, indent, false);
+}
+
+
+RBTree::Node* RBTree::getUncle(Node* node) {
+	// If no parent or grandparent, then no uncle
+	if (node->parent == nullNode || node->parent->parent == nullNode) {
+		return nullptr;
+	}
+
+	if (node->parent == node->parent->parent->left) // uncle on right
+	{
+		return node->parent->parent->right;
+	}
+	else // uncle on left
+	{
+
+		return node->parent->parent->left;
+	}
+}
+
+RBTree::Node* RBTree::rightRotate(Node* x) {
+	Node* y = x->left;
+	Node* T = y->right;
+
+	// Rotation
+	y->parent = x->parent;
+	y->right = x;
+	x->parent = y;
+	x->left = T;
+
+	if (T != nullNode) {
+		T->parent = x;
+	}
+
+	if (x == root) {
+		root = y;
+		return y;
+	}
+
+	if (y->parent->left == x) {
+		y->parent->left = y;
+	}
+	else {
+		y->parent->right = y;
+	}
+
+	return y; // Return new root
+}
+
+RBTree::Node* RBTree::leftRotate(Node* x) {
+	Node* y = x->right;
+	Node* T = y->left;
+
+	// Rotation
+	y->parent = x->parent;
+	y->left = x;
+	x->parent = y;
+	x->right = T;
+
+	if (T != nullNode) {
+		T->parent = x;
+	}
+
+	if (x == root) {
+		root = y;
+		return y;
+	}
+
+	if (y->parent->left == x) {
+		y->parent->left = y;
+	}
+	else {
+		y->parent->right = y;
+	}
+
+	return y; // Return new root
+}
+
+RBTree::Node* RBTree::rightLeftRotate(Node* node) {
+	node->right = rightRotate(node->right);
+	return leftRotate(node);
+}
+
+RBTree::Node* RBTree::leftRightRotate(Node* node) {
+	node->left = leftRotate(node->left);
+	return rightRotate(node);
+}
+
+RBTree::Node* RBTree::find(int data) {
+	Node* curr = root;
+	while (curr != nullNode) {
+		if (curr->data == data) {
+			return curr;
+		}
+		else if (curr->data > data) {
+			curr = curr->left;
+		}
+		else {
+			curr = curr->right;
+		}
+	}
+	return nullptr;
+}
+
+void RBTree::insert(int data) {
+	root = insert(root, data);
+	Node* temp = find(data);
+	fixRedRed(temp);
+}
+
+RBTree::Node* RBTree::insert(Node* node, int data) {
+	if (node == nullNode) {
+		node = new Node(data, nullNode);
+	}
+	else if (node->data > data) {
+		node->left = insert(node->left, data);
+		node->left->parent = node;
+	}
+	else if (node->data < data) {
+		node->right = insert(node->right, data);
+		node->right->parent = node;
+	}
+	return node;
+}
+
+void RBTree::fixRedRed(Node* x) {
+	// if x is root colour it black and return
+	if (x == root) {
+		x->colour = false;
+		return;
+	}
+
+	if (x->parent == nullNode || x->parent->parent == nullNode) {
+		return;
+	}
+	// Initialize parent, grandparent, uncle
+	Node* parent = x->parent, * grandparent = parent->parent, * uncle = getUncle(
+		x);
+	Node* mid = nullptr;
+
+	if (parent->colour == false) {
+		return;
+	}
+
+	// parent colour is red. gp is black.
+	if (uncle != nullNode && uncle->colour == true) {
+		// uncle and parent is red.
+		parent->colour = false;
+		uncle->colour = false;
+		grandparent->colour = true;
+		fixRedRed(grandparent);
+		return;
+	}
+
+	// parent is red, uncle is black and gp is black.
+	// Perform LR, LL, RL, RR
+	if (parent == grandparent->left && x == parent->left) // LL
+	{
+		mid = rightRotate(grandparent);
+	}
+	else if (parent == grandparent->left && x == parent->right) // LR
+	{
+		mid = leftRightRotate(grandparent);
+	}
+	else if (parent == grandparent->right && x == parent->left) // RL
+	{
+		mid = rightLeftRotate(grandparent);
+	}
+	else if (parent == grandparent->right && x == parent->right) // RR
+	{
+		mid = leftRotate(grandparent);
+	}
+
+	mid->colour = false;
+	mid->left->colour = true;
+	mid->right->colour = true;
+}
+
+void RBTree::remove(int data) {
+	remove(this->root, data);
+}
+
+void RBTree::remove(Node* node, int key) {
+	Node* z = nullNode;
+	Node* x, * y;
+	while (node != nullNode) {
+		if (node->data == key) {
+			z = node;
+			break;
+		}
+		else if (node->data <= key) {
+			node = node->right;
+		}
+		else {
+			node = node->left;
+		}
+	}
+
+	if (z == nullNode) {
+		std::cout << "Couldn't find key in the tree" << std::endl;
+		return;
+	}
+
+	y = z;
+	bool yColour = y->colour;
+	if (z->left == nullNode) {
+		x = z->right;
+		joinParentChild(z, z->right);
+	}
+	else if (z->right == nullNode) {
+		x = z->left;
+		joinParentChild(z, z->left);
+	}
+	else {
+		y = minimum(z->right);
+		yColour = y->colour;
+		z->data = y->data;
+		joinParentChild(y, y->right);
+		x = y->right;
+	}
+
+	if (yColour == false) {
+		if (x->colour == true) {
+			x->colour = false;
+			return;
+		}
+		else {
+			fixDoubleBlack(x);
+		}
+	}
+}
+
+void RBTree::fixDoubleBlack(Node* x) {
+	if (x == root) // Root node.
+	{
+		return;
+	}
+
+	Node* sib = getSibling(x);
+	Node* parent = x->parent;
+	if (sib == nullNode) {
+		// No sibling double black shifted to parent.
+		fixDoubleBlack(parent);
+	}
+	else {
+		if (sib->colour == true) {
+			// Sibling colour is red.
+			parent->colour = true;
+			sib->colour = false;
+			if (sib->parent->left == sib) {
+				// Sibling is left child.
+				rightRotate(parent);
+			}
+			else {
+				// Sibling is right child.
+				leftRotate(parent);
+			}
+			fixDoubleBlack(x);
+		}
+		else {
+			// Sibling colour is black
+			// At least one child is red.
+			if (sib->left->colour == true || sib->right->colour == true) {
+				if (sib->parent->left == sib) {
+					// Sibling is left child.
+					if (sib->left != nullNode && sib->left->colour == true) {
+						// left left case.
+						sib->left->colour = sib->colour;
+						sib->colour = parent->colour;
+						rightRotate(parent);
+					}
+					else {
+						// left right case.
+						sib->right->colour = parent->colour;
+						leftRotate(sib);
+						rightRotate(parent);
+					}
+				}
+				else {
+					// Sibling is right child.
+					if (sib->left != nullNode && sib->left->colour == true) {
+						// right left case.
+						sib->left->colour = parent->colour;
+						rightRotate(sib);
+						leftRotate(parent);
+					}
+					else {
+						// right right case.
+						sib->right->colour = sib->colour;
+						sib->colour = parent->colour;
+						leftRotate(parent);
+					}
+				}
+				parent->colour = false;
+			}
+			else {
+				// Both children black.
+				sib->colour = true;
+				if (parent->colour == false) {
+					fixDoubleBlack(parent);
+				}
+				else {
+					parent->colour = false;
+				}
+			}
+		}
+	}
+}
+
+RBTree::Node* RBTree::getSibling(Node* node) {
+	// sibling null if no parent
+	if (node->parent == nullNode) {
+		return nullptr;
+	}
+
+	if (node->parent->left == node) {
+		return node->parent->right;
+	}
+
+	return node->parent->left;
+}
+
+void RBTree::joinParentChild(Node* u, Node* v) {
+	if (u->parent == nullNode) {
+		root = v;
+	}
+	else if (u == u->parent->left) {
+		u->parent->left = v;
+	}
+	else {
+		u->parent->right = v;
+	}
+	v->parent = u->parent;
+}
+
+RBTree::Node* RBTree::minimum(Node* node) {
+	while (node->left != nullNode) {
+		node = node->left;
+	}
+	return node;
+}
+
+// Testing code.
+int main() {
+	RBTree* tree = new RBTree();
+	tree->insert(1);
+	tree->insert(2);
+	tree->insert(3);
+	tree->insert(4);
+	tree->insert(5);
+	tree->insert(7);
+	tree->insert(6);
+	tree->insert(8);
+	tree->insert(9);
+	tree->printTree();
+	tree->remove(4);
+	tree->printTree();
+	delete tree;
+}
+
+/*
+R:4(0)
+   L:2(1)
+   |  L:1(0)
+   |  R:3(0)
+   R:6(1)
+	  L:5(0)
+	  R:8(0)
+		 L:7(1)
+		 R:9(1)
+R:5(0)
+   L:2(1)
+   |  L:1(0)
+   |  R:3(0)
+   R:7(1)
+	  L:6(0)
+	  R:8(0)
+		 R:9(1)
+*/
