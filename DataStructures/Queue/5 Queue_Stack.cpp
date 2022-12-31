8d324760e35ba4216bec35178f03ee467c1d2561
Amit Kumar Parida <aamit.parida@outlook.com>
2022-12-31 20:40:36 +0530

    .

--- DataStructures/Queue/5 Queue_Stack.cpp
+++ DataStructures/Queue/5 Queue_Stack.cpp
@@ -1,54 +1,153 @@
-//Implementation of Queue using Stack
+// Implementation of Queue using Stack
+// https://www.geeksforgeeks.org/queue-using-stacks/
 
 #include <iostream>
 #include <stack>
 using namespace std;
 
-class QueueUsingStack {
-private:
-	stack<int> stk1;
-	stack<int> stk2;
+// Program to implement Queue using two stacks with costly enQueue()
+// Time Complexity : 
+//   Push operation : O(N). In the worst case we have empty whole of stack 1 into stack 2.
+//   Pop operation : O(1). Same as pop operation in stack.
+// Auxiliary Space : O(N). Use of stack for storing values.
+ 
+struct Queue {
+    stack<int> s1, s2;
 
-public:
-	void add(int value);
-	int remove();
+    void enQueue(int x)
+    {
+        // Move all elements from s1 to s2
+        while (!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+
+        // Push item into s1
+        s1.push(x);
+
+        // Push everything back to s1
+        while (!s2.empty()) {
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
+
+    // Dequeue an item from the queue
+    int deQueue()
+    {
+        // if first stack is empty
+        if (s1.empty()) {
+            cout << "Q is Empty";
+            exit(0);
+        }
+
+        // Return top of s1
+        int x = s1.top();
+        s1.pop();
+        return x;
+    }
 };
 
-void QueueUsingStack::add(int value) {
-	stk1.push(value);
+
+// Program to implement Queue using two stacks with costly deQueue()
+
+//Time Complexity :
+//  Push operation : O(1). Same as pop operation in stack.
+//  Pop operation : O(N) in generaland O(1) amortized time complexity.
+//  In the worst case we have to empty the whole of stack 1 into stack 2 so its O(N). Amortized time is the way to express the time complexity when an algorithm has the very bad time complexity only once in a while besides the time complexity that happens most of time.So its O(1) amortized time complexity, since we have to empty whole of stack 1 only when stack 2 is empty, rest of the times the pop operation takes O(1) time.
+//Auxiliary Space : O(N). Use of stack for storing values.
+
+struct Queue1 {
+    stack<int> s1, s2;
+
+    // Enqueue an item to the queue
+    void enQueue(int x)
+    {
+        // Push item into the first stack
+        s1.push(x);
+    }
+
+    // Dequeue an item from the queue
+    int deQueue()
+    {
+        // if both stacks are empty
+        if (s1.empty() && s2.empty()) {
+            cout << "Q is empty";
+            exit(0);
+        }
+
+        // if s2 is empty, move
+        // elements from s1
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        // return the top item from s2
+        int x = s2.top();
+        s2.pop();
+        return x;
+    }
+};
+
+
+// Program to implement Queue using one stack and recursive call stack.
+//Time Complexity :
+//  Push operation : O(1). Same as pop operation in stack.
+//  Pop operation : O(N). The difference from above method is that in this method element is returnedand all elements are restored back in a single call.
+//Auxiliary Space : O(N). Use of stack for storing values.
+
+struct Queue2 {
+    stack<int> s;
+
+    // Enqueue an item to the queue
+    void enQueue(int x)
+    {
+        s.push(x);
+    }
+
+    // Dequeue an item from the queue
+    int deQueue()
+    {
+        if (s.empty()) {
+            cout << "Q is empty";
+            exit(0);
+        }
+
+        // pop an item from the stack
+        int x = s.top();
+        s.pop();
+
+        // if stack becomes empty, return
+        // the popped item
+        if (s.empty())
+            return x;
+
+        // recursive call
+        int item = deQueue();
+
+        // push popped item back to the stack
+        s.push(x);
+
+        // return the result of deQueue() call
+        return item;
+    }
+};
+
+
+// Driver code
+int main()
+{
+    Queue2 q;
+    q.enQueue(1);
+    q.enQueue(2);
+    q.enQueue(3);
+
+    cout << q.deQueue() << '\n';
+    cout << q.deQueue() << '\n';
+    cout << q.deQueue() << '\n';
+
+    return 0;
 }
-
-int QueueUsingStack::remove() {
-	int value;
-	if (stk2.empty() == false) {
-		value = stk2.top();
-		stk2.pop();
-		return value;
-	}
-
-	while (stk1.empty() == false) {
-		value = stk1.top();
-		stk1.pop();
-		stk2.push(value);
-	}
-
-	value = stk2.top();
-	stk2.pop();
-	return value;
-}
-
-// Testing code.
-int main() {
-	QueueUsingStack que;
-	que.add(1);
-	que.add(2);
-	que.add(3);
-	cout << "Queue remove : " << que.remove() << endl;
-	cout << "Queue remove : " << que.remove() << endl;
-	que.add(4);
-	que.add(5);
-	cout << "Queue remove : " << que.remove() << endl;
-
-	return 0;
-}
-
--- DataStructures/Stack/3 Stack_Queue.cpp
+++ DataStructures/Stack/3 Stack_Queue.cpp
@@ -1,63 +1,80 @@
 //Implement Stack using Queue
+// https://www.geeksforgeeks.org/implement-stack-using-queue/
 
 #include <iostream>
 #include <queue>
 using namespace std;
 
+//Implement Stack using 1 Queue
+//Time Complexity :
+//  Push operation : O(N)
+//  Pop operation : O(1)
+//Auxiliary Space : O(N) since 1 queue is used.
+
 class Stack {
-private:
-    queue<int>* que1 = new std::queue<int>();
-    queue<int>* que2 = new std::queue<int>();
-    int size = 0;
+    queue<int> q;
 
 public:
-    virtual void push(int value);
-    virtual int pop();
-    virtual int pop2();
+    void push(int data);
+    void pop();
+    int top();
+    int size();
+    bool empty();
 };
 
+// Push operation
+void Stack::push(int data)
+{
+    //  Get previous size of queue
+    int s = q.size();
 
-void Stack::push(int value) {
-    que1->push(value);
-    size += 1;
-}
+    // Push the current element
+    q.push(data);
 
-int Stack::pop() {
-    int value = 0, s = size;
-    while (s > 0) {
-        value = que1->front();
-        que1->pop();
-        if (s > 1)
-            que2->push(value);
-        s--;
+    // Pop all the previous elements and put them after current element
+    for (int i = 0; i < s; i++) {
+        // Add the front element again
+        q.push(q.front());
+
+        // Delete front element
+        q.pop();
     }
-    queue<int>* temp = que1;
-    que1 = que2;
-    que2 = temp;
-    size -= 1;
-    return value;
 }
 
-int Stack::pop2() {
-    int value = 0, s = size;
-    while (s > 0) {
-        value = que1->front();
-        que1->pop();
-        if (s > 1)
-            que1->push(value);
-        s--;
-    }
-    size -= 1;
-    return value;
+// Removes the top element
+void Stack::pop()
+{
+    if (q.empty())
+        cout << "No elements\n";
+    else
+        q.pop();
 }
 
-// Testing code.
-int main() {
-    Stack stk;
-    stk.push(1);
-    stk.push(2);
-    stk.push(3);
-    cout << "Stack pop : " << stk.pop2() << endl;
-    cout << "Stack pop : " << stk.pop2() << endl;
+int Stack::top() { 
+    return (q.empty()) ? -1 : q.front(); 
+}
+
+// Returns true if Stack is empty else false
+bool Stack::empty() { 
+    return (q.empty()); 
+}
+
+int Stack::size() { 
+    return q.size(); 
+}
+
+int main()
+{
+    Stack st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    cout << "current size: " << st.size() << "\n";
+    cout << st.top() << "\n";
+    st.pop();
+    cout << st.top() << "\n";
+    st.pop();
+    cout << st.top() << "\n";
+    cout << "current size: " << st.size();
     return 0;
 }
