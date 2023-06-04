32ea7fd5b2cbba7ea20ad2a7b16099fa09439528
amitkparida <aamit.parida@outlook.com>
2023-06-04 17:15:45 +0530

    .

--- DataStructures/Graph/1 Graph.cpp
+++ DataStructures/Graph/1 Graph.cpp
@@ -1,1 +1,20 @@
+// TOPIC: Graph Data Structure
+
+// NOTES:
+// 1. A Graph is a non-linear data structure consisting of vertices/nodes and edges/arcs. 
+//    The edges are lines that connect any two vertices in the graph.
+// 
+// 2. Generally, a graph G is represented as G = (V, E), where
+//    - V is set of vertices
+//    - E is set of edges, 
+//
+// 3. An edge is represented as an ordered pair of vertices (u, v) where u is source vertex and v is destination vertex.
+
+// REPRESENTATION OF A GRAPH:
+// The following two are the most commonly used representations of a graph:
+//  - Adjacency Matrix
+//  - Adjacency List
+
+
+// sample codes:
 // https://leetcode.com/discuss/study-guide/2043791/graph-all-in-one-must-watch-for-beginners
